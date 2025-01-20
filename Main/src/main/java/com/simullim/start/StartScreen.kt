10828f1f1f34a94fc3b42a6cd0510221d6a92643
kim0hoon <gns9460@gmail.com>
2025-01-20 23:29:36 +0900

    미사용 import 제거

--- Main/src/main/java/com/simullim/start/StartScreen.kt
+++ Main/src/main/java/com/simullim/start/StartScreen.kt
@@ -1,9 +1,7 @@
 package com.simullim.start
 
-import androidx.compose.foundation.clickable
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 
 @Composable
 fun StartScreen() {
