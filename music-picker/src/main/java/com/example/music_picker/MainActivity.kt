551978dcae0eac3ea22ecc7397107b44510c607d
kim0hoon <gns9460@gmail.com>
2025-01-26 17:48:41 +0900

    plus 버튼 추가

--- music-picker/src/main/java/com/example/music_picker/MainActivity.kt
+++ music-picker/src/main/java/com/example/music_picker/MainActivity.kt
@@ -41,7 +41,11 @@ class MainActivity : ComponentActivity() {
                     title = stringResource(R.string.playlist),
                     leftIcon = CommonHeaderIcon(
                         drawableRes = com.example.common.R.drawable.baseline_arrow_back_ios_new_24,
-                        onClick = { finish() })
+                        onClick = { finish() }),
+                    rightIcon = CommonHeaderIcon(
+                        drawableRes = com.example.common.R.drawable.ic_add_24,
+                        onClick = {}
+                    )
                 )
                 HorizontalDivider(
                     color = Color.White,
