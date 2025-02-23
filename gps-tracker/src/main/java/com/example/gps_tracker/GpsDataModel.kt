3b3d7f38a39b888578157beb111765437dc423c2
kim0hoon <gns9460@gmail.com>
2025-02-23 14:40:21 +0900

    GPS update 구현

--- gps-tracker/src/main/java/com/example/gps_tracker/GpsDataModel.kt
+++ gps-tracker/src/main/java/com/example/gps_tracker/GpsDataModel.kt
@@ -1,7 +1,14 @@
 package com.example.gps_tracker
 
+/**
+ * @param currentVelocity : meter per sec
+ * @param averageVelocity : meter per sec
+ * @param totalDistanceMeter : meter
+ * @param totalTimeMills : millisecond
+ */
 data class GpsDataModel(
-    val currentVelocity: Double = 0.0,
-    val averageVelocity: Double = 0.0,
-    val totalDistanceMeter: Double = 0.0
+    val currentVelocity: Float = 0f,
+    val averageVelocity: Float = 0f,
+    val totalDistanceMeter: Float = 0f,
+    val totalTimeMills: Long = 0L
 )
--- gps-tracker/src/main/java/com/example/gps_tracker/GpsTracker.kt
+++ gps-tracker/src/main/java/com/example/gps_tracker/GpsTracker.kt
@@ -1,35 +1,56 @@
 package com.example.gps_tracker
 
 import android.Manifest.permission.ACCESS_FINE_LOCATION
+import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
-import android.util.Log
 import androidx.annotation.RequiresPermission
+import com.simullim.millsToSec
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class GpsTracker(private val locationManager: LocationManager) {
     private val _gpsDataStateFlow = MutableStateFlow(GpsDataModel())
     val gpsDataStateFlow = _gpsDataStateFlow.asStateFlow()
-    private val locationListener =
-        LocationListener { location ->
-            location.run {
-                Log.d("TESTLOG", "accuracy : ${speedAccuracyMetersPerSecond} speed : $speed")
-
-            }
-        }
+    var lastLocation: Location? = null
+        private set
+    private val locationListener = LocationListener(::updateWithEmitLocation)
+    private var getCurrentLocationJob: Job? = null
 
     @RequiresPermission(ACCESS_FINE_LOCATION)
     fun start() {
         locationManager.requestLocationUpdates(
             LocationManager.GPS_PROVIDER,
-            3000,
+            2000,
             10f,
             locationListener
         )
     }
 
-    fun release() {
+    fun stop() {
         locationManager.removeUpdates(locationListener)
+    }
+
+    private fun updateWithEmitLocation(location: Location) {
+        _gpsDataStateFlow.update {
+            val distance =
+                lastLocation?.let { lastLocation -> location.distanceTo(lastLocation) } ?: 0f
+            val currentSpeed = location.speed
+            val totalDistance = it.totalDistanceMeter + distance
+            val totalTime =
+                it.totalTimeMills + (lastLocation?.let { lastLocation -> location.time - lastLocation.time }
+                    ?: 0)
+            val averageVelocity = totalDistance / millsToSec(totalTime)
+            GpsDataModel(
+                currentVelocity = currentSpeed,
+                averageVelocity = averageVelocity,
+                totalDistanceMeter = totalDistance,
+                totalTimeMills = totalTime
+            )
+        }
+
+        lastLocation = location
     }
 }
