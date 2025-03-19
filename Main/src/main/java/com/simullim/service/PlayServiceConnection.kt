c2632ac696e3f97d14457e9c44eace6b471fa642
kim0hoon <gns9460@gmail.com>
2025-03-19 20:44:24 +0900

    bound 관련 flag 추가

--- Main/src/main/java/com/simullim/service/PlayServiceConnection.kt
+++ Main/src/main/java/com/simullim/service/PlayServiceConnection.kt
@@ -4,15 +4,20 @@ import android.content.ComponentName
 import android.content.ServiceConnection
 import android.os.IBinder
 
-internal class PlayServiceConnection : ServiceConnection {
+internal class PlayServiceConnection(
+    private val onConnected: () -> Unit,
+    private val onDisConnected: () -> Unit
+) : ServiceConnection {
     private var playServiceBinder: PlayService.PlayServiceBinder? = null
     val service get() = playServiceBinder?.getService()
 
     override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
         playServiceBinder = service as? PlayService.PlayServiceBinder
+        onConnected()
     }
 
     override fun onServiceDisconnected(name: ComponentName?) {
         playServiceBinder = null
+        onDisConnected()
     }
 }
