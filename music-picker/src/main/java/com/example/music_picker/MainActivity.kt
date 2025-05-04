5e056eef9cd133bc0ca5382af293caaf4456f901
kim0hoon <gns9460@gmail.com>
2025-05-04 23:46:47 +0900

    다른 액티비티에 음악 읽기 권한 유지하여 전달

--- music-picker/src/main/java/com/example/music_picker/MainActivity.kt
+++ music-picker/src/main/java/com/example/music_picker/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.music_picker
 
+import android.content.ClipData
+import android.content.ClipDescription
 import android.content.Intent
 import android.media.MediaMetadataRetriever
 import android.os.Bundle
@@ -21,6 +23,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.music_picker.model.MusicModel
 import com.example.music_picker.model.PlayItem
@@ -121,6 +124,17 @@ class MainActivity : ComponentActivity() {
                         isEnabled = selected.isNotEmpty(),
                         onClick = {
                             val result = Intent().apply {
+                                val description = ClipDescription("audio", arrayOf())
+                                val items =
+                                    ArrayList(selected.map { ClipData.Item(it.musicModel.uriString.toUri()) })
+                                items.firstOrNull()?.let {
+                                    clipData = ClipData(description, it).apply {
+                                        items.forEachIndexed { index, item ->
+                                            if (index > 0) addItem(item)
+                                        }
+                                    }
+                                }
+                                flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
                                 putParcelableArrayListExtra(
                                     PLAYLIST_RESULT_KEY,
                                     ArrayList(selected.map { it.musicModel })
